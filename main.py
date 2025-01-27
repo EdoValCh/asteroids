173c8298f8cd1c4014a037565478e5e98457c357
EdoValCh <eduardovalenzuelach@gmail.com>
2025-01-27 03:06:40 -0300

    Proyecto finalizado

--- main.py
+++ main.py
@@ -1,21 +1,51 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+import sys
 import pygame
 from constants import *
-
+from circleshape import *
+from player import *
+from asteroid import *
+from asteroidfield import *
+from Shot import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock  = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Shot.containers = (shots, updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable,drawable)
+    AsteroidField.containers = (updatable)
+    asteroidfield = AsteroidField()
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        for sprite in updatable:
+            sprite.update(dt)
+        for asteroid in asteroids:
+                if player.collision(asteroid):
+                    print("Game over!")
+                    sys.exit()
+                for shot in  shots:
+                    if asteroid.collision(shot):
+                        shot.kill()
+                        asteroid.split()
+        for drawable_obj in drawable:
+            drawable_obj.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
-    main()
+    main()  
